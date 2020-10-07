e8809f0416989b410549612aa8281766292ef2bb
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-07 01:38:18 -0300

    Open startup file upon selection
    A new build task project allows us to open the startup file
    after the build finishes (this proved to be the most reliable
    way that didn't cause editor instabilities).
    
    The behavior can be turned off by setting the MSBuild property
    `OpenStartupFile` to false.
    
    The logic seems convoluted, but it's really the only way. The
    Roslyn stuff runs out of process from VS and therefore cannot
    interact with it in any (normal) way. We take inspiration from
    the xunit.vsix project (see https://github.com/kzu/xunit.vsix/blob/master/src/Xunit.Vsix/RunningObjects.cs)
    to basically lookup via COM the running DTE for the active window
    (since you have just changed the startup profile, this will almost
    surely be the process we're interested in) and just run a DTE
    `File.Open` command against it. This has the benefit of focusing
    the tab if it's already opened but not focused, btw.
    
    We keep the last opened file in a flag file too, so as not to
    force open a file that you have explicitly closed after the
    initial auto-open, to avoid being too annoying.
    
    This flag file is *not* added to the FileWrites because we don't
    want a rebuild to cause files to re-open unnecessarily.

---
+++ src/SmallSharp.Build/WindowsInterop.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace SmallSharp
+{
+    static class WindowsInterop
+    {
+        static readonly Regex versionExpr = new Regex(@"Microsoft Visual Studio (?<version>\d\d\.\d)", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+
+        public static void EnsureOpened(string filePath, TimeSpan delay = default)
+        {
+            if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+                return;
+
+            if (delay != default)
+                Thread.Sleep(delay);
+
+            try
+            {
+                var dte = GetDTE();
+                if (dte == null)
+                    return;
+
+                dte.ExecuteCommand("File.OpenFile", filePath);
+            }
+            catch 
+            {
+                Debug.Fail($"Failed to open {filePath}.");
+            }
+        }
+
+        static EnvDTE.DTE? GetDTE()
+        {
+            var window = NativeMethods.GetForegroundWindow();
+            var process = Process.GetProcessesByName("devenv").FirstOrDefault(x => x.MainWindowHandle == window);
+
+            if (process == null)
+                return null;
+
+            var devEnv = process.MainModule.FileName;
+            var version = versionExpr.Match(devEnv).Groups["version"];
+            if (!version.Success)
+            {
+                var ini = Path.ChangeExtension(devEnv, "isolation.ini");
+                if (!File.Exists(ini))
+                    throw new NotSupportedException("Could not determine Visual Studio version from running process from " + devEnv);
+
+                if (!Version.TryParse(File
+                        .ReadAllLines(ini)
+                        .Where(line => line.StartsWith("InstallationVersion=", StringComparison.Ordinal))
+                        .FirstOrDefault()?
+                        .Substring(20), out var v))
+                    throw new NotSupportedException("Could not determine the version of Visual Studio from devenv.isolation.ini at " + ini);
+
+                return GetComObject<EnvDTE.DTE>(string.Format("!{0}.{1}.0:{2}",
+                    "VisualStudio.DTE", v.Major, process.Id), TimeSpan.FromSeconds(2));
+            }
+            else
+            {
+                return GetComObject<EnvDTE.DTE>(string.Format("!{0}.{1}:{2}",
+                    "VisualStudio.DTE", version.Value, process.Id), TimeSpan.FromSeconds(2));
+            }
+        }
+
+        static T? GetComObject<T>(string monikerName, TimeSpan retryTimeout)
+        {
+            object? comObject;
+            var stopwatch = Stopwatch.StartNew();
+            do
+            {
+                comObject = GetComObject(monikerName);
+                if (comObject != null)
+                    break;
+
+                System.Threading.Thread.Sleep(100);
+            }
+
+            while (stopwatch.Elapsed < retryTimeout);
+
+            return (T)comObject;
+        }
+
+        static object? GetComObject(string monikerName)
+        {
+            object? comObject = null;
+            try
+            {
+                IRunningObjectTable table;
+                IEnumMoniker moniker;
+                if (NativeMethods.Failed(NativeMethods.GetRunningObjectTable(0, out table)))
+                    return null;
+
+                table.EnumRunning(out moniker);
+                moniker.Reset();
+                var pceltFetched = IntPtr.Zero;
+                var rgelt = new IMoniker[1];
+
+                while (moniker.Next(1, rgelt, pceltFetched) == 0)
+                {
+                    IBindCtx ctx;
+                    if (!NativeMethods.Failed(NativeMethods.CreateBindCtx(0, out ctx)))
+                    {
+                        string displayName;
+                        rgelt[0].GetDisplayName(ctx, null, out displayName);
+                        if (displayName == monikerName)
+                        {
+                            table.GetObject(rgelt[0], out comObject);
+                            return comObject;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+
+            return comObject;
+        }
+    }
+
+    internal class OleServiceProvider : IServiceProvider
+    {
+        readonly Microsoft.VisualStudio.OLE.Interop.IServiceProvider serviceProvider;
+
+        public OleServiceProvider(Microsoft.VisualStudio.OLE.Interop.IServiceProvider serviceProvider)
+            => this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+
+        public OleServiceProvider(EnvDTE.DTE dte)
+            : this((Microsoft.VisualStudio.OLE.Interop.IServiceProvider)dte) { }
+
+        public object? GetService(Type serviceType)
+            => GetService((serviceType ?? throw new ArgumentNullException(nameof(serviceType))).GUID);
+
+        object? GetService(Guid guid)
+        {
+            if (guid == Guid.Empty)
+                return null;
+
+            if (guid == NativeMethods.IID_IServiceProvider)
+                return serviceProvider;
+
+            try
+            {
+                var riid = NativeMethods.IID_IUnknown;
+                if (NativeMethods.Succeeded(serviceProvider.QueryService(ref guid, ref riid, out var zero)) && (IntPtr.Zero != zero))
+                {
+                    try
+                    {
+                        return Marshal.GetObjectForIUnknown(zero);
+                    }
+                    finally
+                    {
+                        Marshal.Release(zero);
+                    }
+                }
+            }
+            catch (Exception exception) when (
+                exception is OutOfMemoryException ||
+                exception is StackOverflowException ||
+                exception is AccessViolationException ||
+                exception is AppDomainUnloadedException ||
+                exception is BadImageFormatException ||
+                exception is DivideByZeroException)
+            {
+                throw;
+            }
+
+            return null;
+        }
+    }
+}
