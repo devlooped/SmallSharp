a253abd81142412b13ba2996d9c6029c3bb93c7d
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-07 01:38:18 -0300

    Open startup file upon selection
    A new build task project allows us to open the startup file
    after the build finishes (this proved to be the most reliable
    way that didn't cause editor instabilities).
    
    The behavior can be turned off by setting the MSBuild property
    `OpenStartupFile` to false.
    
    The logic seems convoluted, but it's really the only way. The
    Roslyn stuff runs out of process from VS and therefore cannot
    interact with it in any (normal) way. We take inspiration from
    the xunit.vsix project (see https://github.com/kzu/xunit.vsix/blob/master/src/Xunit.Vsix/RunningObjects.cs)
    to basically lookup via COM the running DTE for the active window
    (since you have just changed the startup profile, this will almost
    surely be the process we're interested in) and just run a DTE
    `File.Open` command against it. This has the benefit of focusing
    the tab if it's already opened but not focused, btw.
    
    We keep the last opened file in a flag file too, so as not to
    force open a file that you have explicitly closed after the
    initial auto-open, to avoid being too annoying.
    
    This flag file is *not* added to the FileWrites because we don't
    want a rebuild to cause files to re-open unnecessarily.

---
+++ src/SmallSharp.Build/DisposableAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SmallSharp.Build
+{
+    class DisposableAction : IDisposable
+    {
+        readonly Action action;
+
+        public DisposableAction(Action action) => this.action = action;
+
+        public void Dispose() => action();
+    }
+}
---
+++ src/SmallSharp.Build/OpenStartupFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using EnvDTE;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace SmallSharp.Build
+{
+    public class OpenStartupFile : Task
+    {
+        [Required]
+        public string? FlagFile { get; set; }
+     
+        public string? StartupFile { get; set; }
+
+        public override bool Execute()
+        {
+            if (FlagFile == null || StartupFile == null)
+                return true;
+
+            if (!File.Exists(FlagFile) ||
+                File.ReadAllText(FlagFile) != StartupFile)
+            {
+                // This defers the opening until the build completes.
+                BuildEngine4.RegisterTaskObject(
+                    StartupFile,
+                    new DisposableAction(() => WindowsInterop.EnsureOpened(StartupFile)),
+                    RegisteredTaskObjectLifetime.Build, false);
+
+                File.WriteAllText(FlagFile, StartupFile);
+            }
+
+            return true;
+        }
+    }
+}
