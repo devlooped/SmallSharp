ebc4e321c429262063fea4f6f7a21b27249e1536
Daniel Cazzulino <daniel@cazzulino.com>
2025-09-24 18:11:05 -0300

    Ensure when running from VS, output supports UTF-8
    The console started by VS (even if it's the terminal one) by default does not seem to be set up to support UTF-8. To be on the conservative side, we just set up the encoding in DEBUG builds so things Just Work from VS by default.
    
    This problem doesn't exist when running from a regular terminal.

---
+++ src/SmallSharp/ConsoleEncodingInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace System;
+
+/// <summary>
+/// Ensures that when running from Visual Studio on Windows, the console encoding is set to UTF-8 
+/// to support full Unicode and emoji output.
+/// </summary>
+class ConsoleEncodingInitializer
+{
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    [ModuleInitializer]
+#pragma warning restore CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    public static void Init()
+    {
+
+#if DEBUG
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            Console.InputEncoding = Console.OutputEncoding = Encoding.UTF8;
+#endif
+    }
+}
